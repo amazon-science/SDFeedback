b411f2f5559bcfafa7ac217a880a29d12d934bda
Xinle Sheila Liu <sliuxl@amazon.com>
2025-05-14 21:56:20 -0700

    nit: Fix compile_proto.sh script.

--- src/self_debug/proto/compile_proto.sh
+++ src/self_debug/proto/compile_proto.sh
@@ -16,6 +16,7 @@ proto_files+=("llm_agent.proto")
 proto_files+=("config.proto")
 proto_files+=("cloudwatch.proto")
 
+protoc_path=$(which protoc)
 for proto_file in "${proto_files[@]}"; do
-    $HOME/.local/bin/protoc $INPUT_DIR/proto/$proto_file --python_out=$OUTPUT_DIR
+    $protoc_path $INPUT_DIR/proto/$proto_file --python_out=$OUTPUT_DIR
 done
